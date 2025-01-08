0f24f0be9e3ead7250019109a31d5a8c68745d8a
Alexander Medved <medved1991alexander@gmail.com>
2025-01-08 18:26:23 +0300

    delete validator

---
+++ src/Services/Catalog/Catalog.API/Validations/DeleteProductCommandValidator.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.API.Validations;
+
+public class DeleteProductCommandValidator : AbstractValidator<DeleteProductCommand>
+{
+    public DeleteProductCommandValidator()
+    {
+        RuleFor(i => i.Id).NotEmpty().WithMessage("Is required");
+    }
+}
